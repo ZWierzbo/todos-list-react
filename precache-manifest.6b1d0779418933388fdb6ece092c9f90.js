9fad50d1bfc81926c7fd9644bac5939a049719fb
Zuzanna Wierzbowska <zuzanna.wierzbowska@gmail.com>
2020-07-22 21:43:33 +0100

    Updates

---
+++ precache-manifest.6b1d0779418933388fdb6ece092c9f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc4266c0ffeefc1a5332a70ffec31b8",
+    "url": "/todos-list-react/index.html"
+  },
+  {
+    "revision": "0e9145151ae4b6ef799d",
+    "url": "/todos-list-react/static/css/main.e38c279b.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todos-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9145151ae4b6ef799d",
+    "url": "/todos-list-react/static/js/main.0cb627b0.chunk.js"
+  },
+  {
+    "revision": "97d8fa25e5bde6a122ac",
+    "url": "/todos-list-react/static/js/runtime-main.bb6efc59.js"
+  }
+]);
